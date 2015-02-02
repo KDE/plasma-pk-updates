a378fbe290d584ad6deba30a6518304b5d78d510
Lukáš Tinkl <lukas@kde.org>
2015-02-02 05:08:19 +0100

    minor: fix license

--- src/plasma/metadata.desktop.cmake
+++ src/plasma/metadata.desktop.cmake
@@ -19,5 +19,5 @@ X-KDE-PluginInfo-Author=Lukáš Tinkl
 X-KDE-PluginInfo-Email=lukas@kde.org
 X-KDE-PluginInfo-Version=@PROJECT_VERSION@
 X-KDE-PluginInfo-Website=https://github.com/caybro/plasma-pk-updates
-X-KDE-PluginInfo-License=GPL3
+X-KDE-PluginInfo-License=GPL2
 X-KDE-PluginInfo-EnabledByDefault=true
