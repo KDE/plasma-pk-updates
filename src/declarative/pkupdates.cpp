KDE/plasma-pk-updates

commit 9e236ec5d4d28d7498dfcc830a95590dececcf02

Use new breeze icons

src/declarative/pkupdates.cpp
@@ -97,12 +97,15 @@ bool PkUpdates::isSystemUpToDate() const
 
 QString PkUpdates::iconName() const
 {
-    if (securityCount() > 0)
-        return "security-low";
-    else if (importantCount() > 0)
-        return "security-medium";
-
-    return "security-high";
+    if (securityCount() > 0) {
+        return "update-hight";
+    } else if (importantCount() > 0) {
+        return "update-medium";
+    } else if (count() > 0) {
+        return "update-low";
+    } else {
+        return "update-none";
+    }
 }
 
 QString PkUpdates::message() const

commit 9662b9c5d83ad0188803830d7cf9a96a2db0d114

Fix typo in icon name
Introduced in commit 9e236ec5d4d28d7498dfcc830a95590dececcf02

REVIEW: 128682

src/declarative/pkupdates.cpp
@@ -98,7 +98,7 @@ bool PkUpdates::isSystemUpToDate() const
 QString PkUpdates::iconName() const
 {
     if (securityCount() > 0) {
-        return "update-hight";
+        return "update-high";
     } else if (importantCount() > 0) {
         return "update-medium";
     } else if (count() > 0) {