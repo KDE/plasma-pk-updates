5459b3b32d76d631ea04a876027d8f9bd2d06097
l10n daemon script <scripty@kde.org>
2017-01-10 12:18:06 +0100

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- src/plasma/metadata.desktop.cmake
+++ src/plasma/metadata.desktop.cmake
@@ -24,6 +24,7 @@ Name[x-test]=xxSoftware Updatesxx
 Name[zh_CN]=软件更新
 
 Comment=Get software updates
+Comment[ast]=Consigue anovamientos de software
 Comment[ca]=Obtén les actualitzacions de programari
 Comment[ca@valencia]=Obtén les actualitzacions de programari
 Comment[cs]=Získejte aktualizace software
